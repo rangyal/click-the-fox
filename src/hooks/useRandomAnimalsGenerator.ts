8e84a1a4a710bc28af334233cbb9f237d5bf6e01
Robert Angyal <robert.r.angyal@gmail.com>
2025-09-02 17:26:34 +0200

    Refactor useRandomAnimals to initialize buffer only once in React Strict mode

--- src/hooks/useRandomAnimalsGenerator.ts
+++ src/hooks/useRandomAnimalsGenerator.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getAnimals } from '../api/animals';
 import type { Animal } from '../api/types';
 import { BufferedAsyncGenerator } from '../utils/buffered-async-generator';
@@ -6,19 +6,30 @@ import { BufferedAsyncGenerator } from '../utils/buffered-async-generator';
 type RandomAnimalsGenerator = ReturnType<typeof useRandomAnimalsGenerator>;
 
 const useRandomAnimalsGenerator = () => {
-  const animalsGenerator = useRef(
-    useMemo(() => new BufferedAsyncGenerator(getRandomAnimals, 10), [])
+  const animalsGeneratorRef = useRef<BufferedAsyncGenerator<Animal[]> | null>(
+    null
   );
+  if (!animalsGeneratorRef.current)
+    animalsGeneratorRef.current = new BufferedAsyncGenerator(
+      getRandomAnimals,
+      10
+    );
   const [animals, setAnimals] = useState<Animal[] | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isInitializing, setIsInitializing] = useState(true);
 
+  const getAnimalsGenerator = () => {
+    if (!animalsGeneratorRef.current)
+      throw new Error('Animals generator not initialized');
+    return animalsGeneratorRef.current;
+  };
+
   const loadAnimals = async () => {
     setIsLoading(true);
     setError(null);
     try {
-      setAnimals(await animalsGenerator.current.next());
+      setAnimals(await getAnimalsGenerator().next());
     } catch (error) {
       setError(error as Error);
     }
@@ -28,7 +39,7 @@ const useRandomAnimalsGenerator = () => {
   useEffect(() => {
     const init = async () => {
       try {
-        await animalsGenerator.current.waitForBuffer();
+        await getAnimalsGenerator().waitForBuffer();
         loadAnimals();
       } catch (error) {
         setError(error as Error);
